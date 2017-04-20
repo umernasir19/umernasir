6d3d2329f9b618aef1814422584d033474c029cd
umer nasir <umernasir19@gmail.com>
2017-04-20 12:02:16 +0500

    20 April

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,21 @@ class ArticlesController < ApplicationController
   def new
   end
  
- def create
-  @article = Article.new(params[:article])
+ 
+
+def create
+  @article = Article.new(article_params)
  
   @article.save
   redirect_to @article
 end
+ 
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+   def show
+    @article = Article.find(params[:id])
+  end
 end
